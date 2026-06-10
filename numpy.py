5b6e946f52a4d96388d74a96411988a8f91a75c6
Monika Chauhan <mc4830913@gmail.com>
2026-06-10 07:07:51 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -83,4 +83,75 @@ print(end_time-start_time)
 
 
 
+lst=[4,5,6,8,2]
+print(lst)
+lst1=lst.copy()
+print(lst1)
+
+
+
+# ##creating 2d array
+arr=np.array([['1','2','3'],['3','4','5']])
+print(arr)
+print(arr.shape)
+print(arr.dtype)
+print(arr.size)
+print(type(arr))
+print(arr.itemsize)
+print("***************")
+
+# #creatind 3d array
+arr_=np.array([[[1,2,3],[3,4,5]],
+              [[1,2,3],[3,4,5]]
+               ,[[1,2,3],[3,4,5]]])
+print(arr_)
+print(arr_.shape)
+print(arr_.dtype)
+print(arr_.size)
+print(type(arr_))
+print(arr_.itemsize)
+
+
+##copy function in lists
+lst=[4,5,6,8,2]
+print(lst)
+
+lst1=lst
+print(lst1)
+print("**********************")
+
+lst[2]=20
+print(lst)
+print(lst1)
+print("**********************")
+
+del(lst[0])
+print(lst)
+print(lst1)
+print("**********************")
+
+lst=[4,5,6,8,2]
+print(lst)
+lst1=lst.copy()
+print(lst1)
+print("**********************")
+
+lst[1]=3
+print(lst)
+print(lst1)
+print("**********************")
+
+
+###copy function in arrays
+arr=np.array([4,5,6,8,2])
+print(arr)
+arr_=arr.copy()
+print(arr_)
+
+arr[3]=10
+print(arr)
+print(arr_)
+
+
+
 
